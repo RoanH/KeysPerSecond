dea873202a36364266f3801f951677c79e832faf
Roan <roan.hofland@hotmail.com>
2017-02-01 18:12:36 +0100

    Clean lib directory

--- KeysPerSecond/lib/jnativehook-2.1.0/jnativehook/src/libuiohook/test/minunit.h
+++
@@ -1,5 +0,0 @@
-// MinUnit -- A minimal unit testing framework for C
-#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-#define mu_run_test(test) do { char *message = test(); tests_run++; if (message) return message; } while (0)
-
-extern int tests_run;
