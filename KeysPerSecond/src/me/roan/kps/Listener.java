ad7ea21fc874c58f5ba81915c2e0d031c9c7a31b
Roan <roan.hofland@hotmail.com>
2017-04-05 20:55:32 +0200

    Fix window moving issue and add 1, 2 & 3 px move speed (-,ctrl,shift)
    fix issue #35

--- KeysPerSecond/src/me/roan/kps/Listener.java
+++ KeysPerSecond/src/me/roan/kps/Listener.java
@@ -73,27 +73,30 @@ public class Listener implements MouseMotionListener, MouseListener{
 			
 			@Override
 			public boolean dispatchKeyEvent(KeyEvent e) {
-				switch(e.getKeyCode()){
-				case KeyEvent.VK_LEFT:
-				case KeyEvent.VK_KP_LEFT:
-					Main.frame.getLocation(tmp);
-					Main.frame.setLocation(tmp.x - 1, tmp.y);
-					break;
-				case KeyEvent.VK_RIGHT:
-				case KeyEvent.VK_KP_RIGHT:
-					Main.frame.getLocation(tmp);
-					Main.frame.setLocation(tmp.x + 1, tmp.y);
-					break;
-				case KeyEvent.VK_UP:
-				case KeyEvent.VK_KP_UP:
-					Main.frame.getLocation(tmp);
-					Main.frame.setLocation(tmp.x, tmp.y - 1);
-					break;
-				case KeyEvent.VK_DOWN:
-				case KeyEvent.VK_KP_DOWN:
-					Main.frame.getLocation(tmp);
-					Main.frame.setLocation(tmp.x, tmp.y + 1);
-					break;
+				if(e.getID() == KeyEvent.KEY_PRESSED){
+					int d = e.isShiftDown() ? 3 : e.isControlDown() ? 2 : 1;
+					switch(e.getKeyCode()){
+					case KeyEvent.VK_LEFT:
+					case KeyEvent.VK_KP_LEFT:
+						Main.frame.getLocation(tmp);
+						Main.frame.setLocation(tmp.x - d, tmp.y);
+						break;
+					case KeyEvent.VK_RIGHT:
+					case KeyEvent.VK_KP_RIGHT:
+						Main.frame.getLocation(tmp);
+						Main.frame.setLocation(tmp.x + d, tmp.y);
+						break;
+					case KeyEvent.VK_UP:
+					case KeyEvent.VK_KP_UP:
+						Main.frame.getLocation(tmp);
+						Main.frame.setLocation(tmp.x, tmp.y - d);
+						break;
+					case KeyEvent.VK_DOWN:
+					case KeyEvent.VK_KP_DOWN:
+						Main.frame.getLocation(tmp);
+						Main.frame.setLocation(tmp.x, tmp.y + d);
+						break;
+					}
 				}
 				return false;
 			}
