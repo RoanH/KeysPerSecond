8338b680e24aafb97d0af043674b9f367564b66b
Roan <roan.hofland@hotmail.com>
2017-04-19 16:38:26 +0200

    Disable arrow key moving of the program when the menu is open
    fix issue #58

--- KeysPerSecond/src/me/roan/kps/Listener.java
+++ KeysPerSecond/src/me/roan/kps/Listener.java
@@ -21,7 +21,7 @@ public class Listener implements MouseMotionListener, MouseListener{
 	 * Instance
 	 */
 	protected static final Listener INSTANCE = new Listener();
-	
+
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		Point to = e.getPoint();
@@ -62,7 +62,7 @@ public class Listener implements MouseMotionListener, MouseListener{
 	@Override
 	public void mouseExited(MouseEvent e) {		
 	}
-	
+
 	static{
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher(){
 			/**
@@ -70,32 +70,34 @@ public class Listener implements MouseMotionListener, MouseListener{
 			 * on each event
 			 */
 			private Point tmp = new Point();
-			
+
 			@Override
 			public boolean dispatchKeyEvent(KeyEvent e) {
-				if(e.getID() == KeyEvent.KEY_PRESSED){
-					int d = e.isShiftDown() ? 3 : e.isControlDown() ? 2 : 1;
-					switch(e.getKeyCode()){
-					case KeyEvent.VK_LEFT:
-					case KeyEvent.VK_KP_LEFT:
-						Main.frame.getLocation(tmp);
-						Main.frame.setLocation(tmp.x - d, tmp.y);
-						break;
-					case KeyEvent.VK_RIGHT:
-					case KeyEvent.VK_KP_RIGHT:
-						Main.frame.getLocation(tmp);
-						Main.frame.setLocation(tmp.x + d, tmp.y);
-						break;
-					case KeyEvent.VK_UP:
-					case KeyEvent.VK_KP_UP:
-						Main.frame.getLocation(tmp);
-						Main.frame.setLocation(tmp.x, tmp.y - d);
-						break;
-					case KeyEvent.VK_DOWN:
-					case KeyEvent.VK_KP_DOWN:
-						Main.frame.getLocation(tmp);
-						Main.frame.setLocation(tmp.x, tmp.y + d);
-						break;
+				if(!Menu.menu.isVisible()){
+					if(e.getID() == KeyEvent.KEY_PRESSED){
+						int d = e.isShiftDown() ? 3 : e.isControlDown() ? 2 : 1;
+						switch(e.getKeyCode()){
+						case KeyEvent.VK_LEFT:
+						case KeyEvent.VK_KP_LEFT:
+							Main.frame.getLocation(tmp);
+							Main.frame.setLocation(tmp.x - d, tmp.y);
+							break;
+						case KeyEvent.VK_RIGHT:
+						case KeyEvent.VK_KP_RIGHT:
+							Main.frame.getLocation(tmp);
+							Main.frame.setLocation(tmp.x + d, tmp.y);
+							break;
+						case KeyEvent.VK_UP:
+						case KeyEvent.VK_KP_UP:
+							Main.frame.getLocation(tmp);
+							Main.frame.setLocation(tmp.x, tmp.y - d);
+							break;
+						case KeyEvent.VK_DOWN:
+						case KeyEvent.VK_KP_DOWN:
+							Main.frame.getLocation(tmp);
+							Main.frame.setLocation(tmp.x, tmp.y + d);
+							break;
+						}
 					}
 				}
 				return false;
