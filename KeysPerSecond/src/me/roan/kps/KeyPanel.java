baa417ee5a59d37d96d3135987456350d5472045
RoanH <RoanH@roan-desktop.home>
2017-06-25 17:20:09 +0200

    Fix horizontal rendering for the base panel

--- KeysPerSecond/src/me/roan/kps/KeyPanel.java
+++ KeysPerSecond/src/me/roan/kps/KeyPanel.java
@@ -62,6 +62,44 @@ public final class KeyPanel extends JPanel {
 	@Override
 	public void paintComponent(Graphics g1) {
 		Graphics2D g = (Graphics2D) g1;
+		if(Main.config.mode == RenderingMode.VERTICAL){
+			verticalRenderer(g);
+		}else{
+			horizontalRenderer(g);
+		}
+	}
+	
+	private final void horizontalRenderer(Graphics2D g){
+		g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC, Main.config.getBackgroundOpacity()));
+		g.setColor(Main.config.getBackgroundColor());
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, Main.config.getForegroundOpacity()));
+		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g.drawImage(ColorManager.unpressed, 2, 2, this.getWidth() - 2, this.getHeight() - 2, 0, 0, 64, 40, this);
+		if (key.down) {
+			g.drawImage(ColorManager.pressed, 2, 2, this.getWidth() - 2, this.getHeight() - 2, 0, 0, 64, 40, this);
+			g.setColor(Main.config.getBackgroundColor());
+		}else{
+			g.setColor(Main.config.getForegroundColor());
+		}
+		if(key.name.length() == 1){
+			g.setFont(font1);
+		}else{
+			g.setFont(BasePanel.font1);
+		}
+		g.drawString(key.name, (this.getWidth() - g.getFontMetrics().stringWidth(key.name)) / 2, SizeManager.keyTitleTextOffset);
+		if(key.count >= 10000){
+			g.setFont(font2smallest);
+		}else if(key.count >= 1000){
+			g.setFont(font2small);
+		}else{
+			g.setFont(font2);
+		}
+		String str = String.valueOf(key.count);
+		g.drawString(str, (this.getWidth() - g.getFontMetrics().stringWidth(str)) / 2, SizeManager.keyDataTextOffset);
+	}
+	
+	private final void verticalRenderer(Graphics2D g){
 		g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC, Main.config.getBackgroundOpacity()));
 		g.setColor(Main.config.getBackgroundColor());
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
