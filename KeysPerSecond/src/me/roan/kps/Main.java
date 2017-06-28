520489935ef79b9dfc7e8f6357c511e175a320a7
Roan <roan.hofland@hotmail.com>
2017-06-28 16:18:54 +0200

    Increment version

--- KeysPerSecond/src/me/roan/kps/Main.java
+++ KeysPerSecond/src/me/roan/kps/Main.java
@@ -731,7 +731,7 @@ public class Main {
 			}
 		});
 		String version = checkVersion();//XXX the version number 
-		JLabel ver = new JLabel("<html><center><i>Version: v6.4, latest version: " + (version == null ? "unknown :(" : version) + "<br>"
+		JLabel ver = new JLabel("<html><center><i>Version: v6.5, latest version: " + (version == null ? "unknown :(" : version) + "<br>"
 				+ "<u><font color=blue> https://osu.ppy.sh/community/forums/topics/552405 </font></u></i></center></html>", SwingConstants.CENTER);
 		ver.addMouseListener(new MouseListener(){
 
