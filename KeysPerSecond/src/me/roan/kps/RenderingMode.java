a17938aecf0afd4a092c00a395fe49f38944d49d
Roan <roan.hofland@hotmail.com>
2017-09-09 17:30:42 +0200

    Refactor & add javadoc to RenderingMode

--- KeysPerSecond/src/me/roan/kps/RenderingMode.java
+++ KeysPerSecond/src/me/roan/kps/RenderingMode.java
@@ -74,24 +74,59 @@ public enum RenderingMode {
 		return name;
 	}
 	
+	/**
+	 * Mode specific logic for the title position
+	 * @param metrics FontMetrics that will be used to draw the title
+	 * @param baseline Baseline that will be used to draw the title
+	 * @param width Panel width
+	 * @param title The title that will be drawn
+	 * @return The location at which the title should be drawn
+	 */
 	protected abstract Point getTitleDrawPositionImpl(FontMetrics metrics, int baseline, int width, String title);
 	
+	/**
+	 * Mode specific logic for the value position
+	 * @param metrics FontMetrics that will be used to draw the value
+	 * @param baseline Baseline that will be used to draw the value
+	 * @param width Panel width
+	 * @param value The value that will be drawn
+	 * @return The location at which the value should be drawn
+	 */
 	protected abstract Point getValueDrawPositionImpl(FontMetrics metrics, int baseline, int width, String value);
 	
+	/**
+	 * Gets the location at which the panel title should be drawn
+	 * @param g The graphics used for drawing
+	 * @param panel The panel
+	 * @param title The title
+	 * @param font The font with which the title is going to be drawn
+	 * @return The location at which the title should be drawn
+	 */
 	public Point getTitleDrawPosition(Graphics2D g, BasePanel panel, String title, Font font){
-		FontMetrics metrics = g.getFontMetrics(font);
-		int baseline = getHorizontalBaseline(g, panel, font);
-		return getTitleDrawPositionImpl(metrics, baseline, panel.getWidth(), title);
+		return getTitleDrawPositionImpl(g.getFontMetrics(font), getHorizontalBaseline(g, panel, font), panel.getWidth(), title);
 	}
 	
+	/**
+	 * Gets the location at which the panel value should be drawn
+	 * @param g The graphics used for drawing
+	 * @param panel The panel
+	 * @param title The value
+	 * @param font The font with which the value is going to be drawn
+	 * @return The location at which the value should be drawn
+	 */
 	public Point getValueDrawPosition(Graphics2D g, BasePanel panel, String value, Font font){
-		FontMetrics metrics = g.getFontMetrics(font);
-		int baseline = getHorizontalBaseline(g, panel, font);
-		return getValueDrawPositionImpl(metrics, baseline, panel.getWidth(), value);
+		return getValueDrawPositionImpl(g.getFontMetrics(font), getHorizontalBaseline(g, panel, font), panel.getWidth(), value);
 	}
 	
-	private int getHorizontalBaseline(Graphics2D g, BasePanel panel, Font f) {
-		FontMetrics metrics = g.getFontMetrics(f);
+	/**
+	 * Gets the baseline to draw text in the panel
+	 * @param g The graphics used for drawing
+	 * @param panel The panel
+	 * @param font The font used for drawing
+	 * @return The horizontal baseline to use for drawing
+	 */
+	private int getHorizontalBaseline(Graphics2D g, BasePanel panel, Font font) {
+		FontMetrics metrics = g.getFontMetrics(font);
 		return (panel.getHeight() + metrics.getAscent() - metrics.getDescent()) / 2;
 	}
 }
