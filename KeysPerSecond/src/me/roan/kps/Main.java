b3dc29deb475ec54e6573677b3e8a20bbe4e36f2
Roan <roan.hofland@hotmail.com>
2018-09-06 00:55:57 +0200

    Cleanup imports

--- KeysPerSecond/src/me/roan/kps/Main.java
+++ KeysPerSecond/src/me/roan/kps/Main.java
@@ -32,7 +32,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
