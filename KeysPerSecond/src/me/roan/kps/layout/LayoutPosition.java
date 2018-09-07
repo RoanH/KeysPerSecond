5bab53900f2c460e9d0177b0525b310d33d2dddd
Roan <roan.hofland@hotmail.com>
2018-09-08 01:40:03 +0200

    Complete the javadoc for the LayoutPosition class

--- KeysPerSecond/src/me/roan/kps/layout/LayoutPosition.java
+++ KeysPerSecond/src/me/roan/kps/layout/LayoutPosition.java
@@ -1,33 +1,47 @@
 package me.roan.kps.layout;
 
 /**
- * //TODO
+ * Interface that panels that will be
+ * added to a {@link Layout} have to implement
  * @author Roan
+ * @see Layout
  */
 public abstract interface LayoutPosition {
-	///**
-	// * Returns the layout index of the component
-	// * @return The layout index
-	// */
-	//@Deprecated
-	//public abstract int getIndex();
-	
+	/**
+	 * Gets the x position of this
+	 * panel in the layout
+	 * @return The x position of this
+	 *         panel in the layout
+	 */
 	public abstract int getLayoutX();
-	
+	/**
+	 * Gets the y position of this
+	 * panel in the layout
+	 * @return The y position of this
+	 *         panel in the layout
+	 */
 	public abstract int getLayoutY();
-	
+	/**
+	 * Gets the width of this panel
+	 * in the layout
+	 * @return The width of this panel
+	 *         in the layout
+	 */
 	public abstract int getLayoutWidth();
-	
+	/**
+	 * Gets the height of this panel
+	 * in the layout
+	 * @return The height of this panel
+	 *         in the layout
+	 */
 	public abstract int getLayoutHeight();
 	
-	public default int getXWidth(){
-		return getLayoutX() + getLayoutWidth();
-	}
-	
-	public default int getYHeight(){
-		return getLayoutY() + getLayoutHeight();
-	}
-	
+	/**
+	 * Gets the position and size given by
+	 * this LayoutPosition as a string
+	 * @return A string giving the position
+	 *         and size of this LayoutPosition
+	 */
 	public default String getLayoutLocation(){
 		return "LayoutPosition[x=" + getLayoutX() + ",y=" + getLayoutY() + ",width=" + getLayoutWidth() + ",height=" + getLayoutHeight() + "]";
 	}
