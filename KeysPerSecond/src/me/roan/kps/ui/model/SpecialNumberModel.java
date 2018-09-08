dd6613fd78224ca120c5b18b75f1f7d37e1a738b
Roan <roan.hofland@hotmail.com>
2018-09-08 02:20:07 +0200

    Add a javadoc to the SpecialNumberModel class

--- KeysPerSecond/src/me/roan/kps/ui/model/SpecialNumberModel.java
+++ KeysPerSecond/src/me/roan/kps/ui/model/SpecialNumberModel.java
@@ -4,17 +4,44 @@ import javax.swing.AbstractSpinnerModel;
 
 import me.roan.kps.layout.LayoutValidator.FieldListener;
 
+/**
+ * Special number model for spinner that allows
+ * for one custom value to be added. This custom
+ * value will take the position of the value -1.
+ * @author Roan
+ * @see EndNumberModel
+ * @see MaxNumberModel
+ */
 public class SpecialNumberModel extends AbstractSpinnerModel{
 	/**
 	 * Serial ID
 	 */
 	private static final long serialVersionUID = 4181535442197142288L;
-	
+	/**
+	 * The current value of this spinner
+	 */
 	private int value;
+	/**
+	 * The name of the special value
+	 */
 	private final String special;
+	/**
+	 * The listener to notify of value changes
+	 */
 	private ValueChangeListener listener;
+	/**
+	 * The validator to use to check the validity of new values
+	 */
 	private FieldListener validator;
 	
+	/**
+	 * Constructs a new SpecialNumberModel with
+	 * the given arguments.
+	 * @param value The initial value for the model
+	 * @param special The name of the special value
+	 * @param validator The validator to use
+	 * @param listener The listener to notify of value changes
+	 */
 	public SpecialNumberModel(int value, String special, FieldListener validator, ValueChangeListener listener){
 		this.value = value;
 		this.special = special;
@@ -23,6 +50,12 @@ public class SpecialNumberModel extends AbstractSpinnerModel{
 		this.listener = listener;
 	}
 	
+	/**
+	 * Checks to see if the current value
+	 * is the special (-1) value
+	 * @return Whether or not the special
+	 *         value is currently selected
+	 */
 	public boolean isSpecialValueSelected(){
 		return value == -1;
 	}
@@ -57,9 +90,17 @@ public class SpecialNumberModel extends AbstractSpinnerModel{
 		return value == -1 ? special : String.valueOf(value);
 	}
 
+	/**
+	 * Listener that is notified of value changes
+	 * for a SpecialNumberModel
+	 * @author Roan
+	 */
 	@FunctionalInterface
 	public static abstract interface ValueChangeListener{
-
+		/**
+		 * Called when a new value was requested
+		 * @param newValue The requested new value
+		 */
 		public abstract void valueChanged(int newValue);
 	}
 }
