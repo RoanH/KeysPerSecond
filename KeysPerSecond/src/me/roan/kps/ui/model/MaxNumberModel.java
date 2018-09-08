95ecc14ec062c1bb3c777086536edd235fc2a895
Roan <roan.hofland@hotmail.com>
2018-09-08 02:22:36 +0200

    Add a javadoc to the EndNumberModel and MaxNumberModel classes
    fixes issue #103

--- KeysPerSecond/src/me/roan/kps/ui/model/MaxNumberModel.java
+++ KeysPerSecond/src/me/roan/kps/ui/model/MaxNumberModel.java
@@ -2,12 +2,25 @@ package me.roan.kps.ui.model;
 
 import me.roan.kps.layout.LayoutValidator.FieldListener;
 
+/**
+ * SpecialNumberModel with <i>max</i>
+ * as the special value
+ * @author Roan
+ * @see SpecialNumberModel
+ */
 public class MaxNumberModel extends SpecialNumberModel{
 	/**
 	 * Serial ID
 	 */
 	private static final long serialVersionUID = -7707259211999223448L;
 
+	/**
+	 * Constructs a new MaxNumberModel with the
+	 * given initial value, validator and listener
+	 * @param value The initial model value
+	 * @param validator The validator to use to validate value changes
+	 * @param listener The listener to inform of value changes
+	 */
 	public MaxNumberModel(int value, FieldListener validator, ValueChangeListener listener) {
 		super(value, "max", validator, listener);
 	}
