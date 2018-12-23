1c03adfb2447452647a5ad092882c820fb1f98df
Roan <roan.hofland@hotmail.com>
2018-12-23 01:31:52 +0100

    Complete the javadoc

--- KeysPerSecond/src/me/roan/kps/Main.java
+++ KeysPerSecond/src/me/roan/kps/Main.java
@@ -2004,28 +2004,68 @@ public class Main{
 	//================== DIALOGUES ====================================================================
 	//=================================================================================================
 
+	/**
+	 * Shows the given object to the user.
+	 * The dialog will have a 'Save' and
+	 * 'Cancel' close option.
+	 * @param form The object to display
+	 * @param resizable Whether the user should
+	 *        be able to resize the dialog
+	 * @return True if the 'Save' option was selected
+	 */
 	protected static final boolean showOptionDialog(Object form, boolean resizable){
 		return showDialog(form, resizable, new String[]{"Save", "Cancel"});
 	}
 	
+	/**
+	 * Shows the given object to the user.
+	 * The dialog will have a 'Save' and
+	 * 'Cancel' close option.
+	 * @param form The object to display
+	 * @return True if the 'Save' option was selected
+	 */
 	protected static final boolean showOptionDialog(Object form){
 		return showOptionDialog(form, false);
 	}
 	
+	/**
+	 * Poses the given object as a yes/no option
+	 * dialog to the user.
+	 * @param msg The object to display
+	 * @return True if the user selected yes
+	 */
 	protected static final boolean showConfirmDialog(Object msg){
 		return showDialog(msg, false, new String[]{"Yes", "No"});
 	}
 	
+	/**
+	 * Shows the given object to the user in a dialog
+	 * @param msg The object to display
+	 */
 	protected static final void showMessageDialog(Object msg){
 		showDialog(msg, false, new String[]{"OK"});
 	}
 	
+	/**
+	 * Shows the given error message to the user
+	 * @param error The error to display
+	 */
 	protected static final void showErrorDialog(String error){
 		showMessageDialog(error);
 	}
 	
-	//TODO javadoc
-	//returns false if cancel == false
+	/**
+	 * Shows the given object as a dialog
+	 * with the given close options
+	 * @param form The dialog to display
+	 * @param resizable Whether or not the
+	 *        dialog can be resized
+	 * @param options The close options
+	 *        for the dialog
+	 * @return True if the used close option
+	 *         is the first item in the close
+	 *         options list, false otherwise
+	 */
 	protected static final boolean showDialog(Object form, boolean resizable, String[] options){
 		JOptionPane optionPane = new JOptionPane(form, JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, 0);
 		JDialog dialog = optionPane.createDialog(frame.isVisible() ? frame : null, "Keys per second");
