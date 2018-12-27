2ed4eb7067f5bd5b64c1b71fca659790a5e46de6
Roan <roan.hofland@hotmail.com>
2018-12-27 16:49:57 +0100

    Make auto increment resetting consistent with normal visible behavior

--- KeysPerSecond/src/me/roan/kps/Menu.java
+++ KeysPerSecond/src/me/roan/kps/Menu.java
@@ -560,7 +560,7 @@ public class Menu{
 		TotPanel.hits = 0;
 		Main.reconfigure();
 		Main.mainLoop();
-		KeyInformation.autoIndex = (int)(Main.config.keyinfo.stream().filter(k->k.visible).count() * 2 - 2);
+		KeyInformation.autoIndex = (int)(Main.config.keyinfo.size() * 2 - 2);
 		Main.graphFrame.setAlwaysOnTop(Main.config.overlay);
 		Main.frame.setAlwaysOnTop(Main.config.overlay);
 	}
