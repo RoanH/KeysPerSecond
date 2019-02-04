df05791976aa2c65cd25ed9f36dc382e1725532b
Roan <roan.hofland@hotmail.com>
2019-02-04 15:07:01 +0100

    Cleanup

--- KeysPerSecond/src/me/roan/kps/Statistics.java
+++ KeysPerSecond/src/me/roan/kps/Statistics.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
