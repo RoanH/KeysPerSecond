5fbda665c18c3931dcd7fd7333286644f5d2ffb8
Roan Hofland <roan.hofland@hotmail.com>
2020-08-26 18:31:16 +0200

    Update average display formatting

--- KeysPerSecond/src/me/roan/kps/panels/AvgPanel.java
+++ KeysPerSecond/src/me/roan/kps/panels/AvgPanel.java
@@ -32,7 +32,7 @@ public final class AvgPanel extends BasePanel{
 
 	@Override
 	protected String getValue(){
-		return Main.config.precision == 0 ? String.valueOf((int)Main.avg) : String.valueOf(String.format("%1$." + Main.config.precision + "f", (double)((int)(Main.avg * Math.pow(10, Main.config.precision)) / Math.pow(10, Main.config.precision))));
+		return String.format("%1$." + Main.config.precision + "f", Main.avg);
 	}
 
 	@Override
