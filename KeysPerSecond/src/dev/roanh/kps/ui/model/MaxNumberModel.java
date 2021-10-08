c5bd9d1de90d90edce3dada31f0e864dea2e730e
Roan Hofland <roan@roanh.dev>
2021-10-09 00:22:20 +0200

    Update package name

---
+++ KeysPerSecond/src/dev/roanh/kps/ui/model/MaxNumberModel.java
@@ -0,0 +1,27 @@
+package dev.roanh.kps.ui.model;
+
+import dev.roanh.kps.layout.LayoutValidator.FieldListener;
+
+/**
+ * SpecialNumberModel with <i>max</i>
+ * as the special value
+ * @author Roan
+ * @see SpecialNumberModel
+ */
+public class MaxNumberModel extends SpecialNumberModel{
+	/**
+	 * Serial ID
+	 */
+	private static final long serialVersionUID = -7707259211999223448L;
+
+	/**
+	 * Constructs a new MaxNumberModel with the
+	 * given initial value, validator and listener
+	 * @param value The initial model value
+	 * @param validator The validator to use to validate value changes
+	 * @param listener The listener to inform of value changes
+	 */
+	public MaxNumberModel(int value, FieldListener validator, ValueChangeListener listener){
+		super(value, "max", validator, listener);
+	}
+}
