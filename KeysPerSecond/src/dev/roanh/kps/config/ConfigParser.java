a890e3c4703de567e164a70d96f46a105179a8c4
Roan Hofland <roan@roanh.dev>
2023-10-24 14:00:53 +0200

    Implement list parsing

--- KeysPerSecond/src/dev/roanh/kps/config/ConfigParser.java
+++ KeysPerSecond/src/dev/roanh/kps/config/ConfigParser.java
@@ -19,11 +19,17 @@
 package dev.roanh.kps.config;
 
 import java.io.BufferedReader;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 public class ConfigParser{
 	private String version;
@@ -44,6 +50,10 @@ public class ConfigParser{
 	private void parse(BufferedReader in) throws IOException{
 		//read version
 		String line = in.readLine();
+		if(line == null){
+			throw new IOException("Empty config file");
+		}
+		
 		if(!line.startsWith("version:")){
 			//the last version to not declare a version
 			version = "v8.4";
@@ -51,6 +61,7 @@ public class ConfigParser{
 			version = line.substring(8).trim();
 		}
 
+		//read data
 		
 		
 		
@@ -61,6 +72,68 @@ public class ConfigParser{
 	private Map<String, Setting<?>> settings;
 	
 	
+	
+	private static final char[] LIST_ITEM_START = new char[]{' ', ' ', '-', ' '};
+	private static final char[] LIST_ITEM_BODY = new char[]{' ', ' ', ' ', ' '};
+	
+	public static void main(String[] args) throws IOException{
+		parseList(Files.newBufferedReader(Paths.get("C:\\Users\\RoanH\\Downloads\\ymltest.txt")), map->{
+			
+			System.out.println("item: " + map);
+			
+			return null;
+		});
+	}
+	
+	private static <T extends SettingGroup> List<T> parseList(BufferedReader in, Function<Map<String, String>, T> ctor) throws IOException{
+		in.mark(1000);
+		char[] lead = new char[4];
+		List<T> data = new ArrayList<T>();
+		
+		Map<String, String> item = null;
+		while(in.ready()){
+			if(in.read(lead, 0, 4) != 4){
+				//end of file hit or not enough list data
+				in.reset();
+				break;
+			}
+			
+			if(Arrays.equals(lead, LIST_ITEM_START)){
+				if(item != null){
+					data.add(ctor.apply(item));
+				}
+				
+				item = new HashMap<String, String>();
+			}else if(!Arrays.equals(lead, LIST_ITEM_BODY)){
+				//end of list
+				in.reset();
+				break;
+			}
+			
+			String line = in.readLine();
+			if(line == null){
+				//end of file
+				in.reset();
+				break;
+			}
+			
+			int mark = line.indexOf(':');
+			if(mark == -1){
+				//assume leading whitespace on the next line
+				in.reset();
+				break;
+			}
+			
+			item.put(line.substring(0, mark).trim(), line.substring(mark + 1, line.length()).trim());
+		}
+		
+		//end last item
+		if(item != null){
+			data.add(ctor.apply(item));
+		}
+		
+		return data;
+	}
 	
 	
 	
