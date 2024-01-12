8d54e657e82e513c4a9081c132c37922448bfa20
Roan Hofland <roan@roanh.dev>
2024-01-12 18:19:05 +0100

    Document the PanelSettings class

--- KeysPerSecond/src/dev/roanh/kps/config/group/PanelSettings.java
+++ KeysPerSecond/src/dev/roanh/kps/config/group/PanelSettings.java
@@ -25,35 +25,81 @@ import dev.roanh.kps.config.IndentWriter;
 import dev.roanh.kps.config.setting.RenderingModeSetting;
 import dev.roanh.kps.config.setting.StringSetting;
 
+/**
+ * Settings for a named value display panel.
+ * @author Roan
+ */
 public abstract class PanelSettings extends LocationSettings{
+	/**
+	 * The name of this panel.
+	 */
 	protected final StringSetting name;
+	/**
+	 * The rendering mode used the panel.
+	 */
 	protected final RenderingModeSetting mode = new RenderingModeSetting("mode", RenderingMode.VERTICAL);
 	
-	public PanelSettings(String key, int x, int y, int width, int height, String defaultName){
+	/**
+	 * Constructs new panel settings.
+	 * @param key The settings group key.
+	 * @param x The x position of the panel.
+	 * @param y The y position of the panel.
+	 * @param width The width of the panel.
+	 * @param height The height of the panel.
+	 * @param defaultName The display name of the panel.
+	 */
+	protected PanelSettings(String key, int x, int y, int width, int height, String defaultName){
 		super(key, x, y, width, height);
 		name = new StringSetting("name", defaultName);
 	}
 	
-	public PanelSettings(String key, String defaultName){
+	/**
+	 * Constructs a new panel with default dimensions.
+	 * @param key The settings group key.
+	 * @param defaultName The display name of the panel.
+	 */
+	protected PanelSettings(String key, String defaultName){
 		this(key, -1, 0, 2, 3, defaultName);
 	}
 
+	/**
+	 * Gets the rendering mode for this panel.
+	 * @return The rendering mode for this panel.
+	 * @see RenderingMode
+	 */
 	public RenderingMode getRenderingMode(){
 		return mode.getValue();
 	}
 
+	/**
+	 * Gets the display name of this panel.
+	 * @return The name of this panel.
+	 */
 	public String getName(){
 		return name.getValue();
 	}
 	
+	/**
+	 * Sets the rendering mode for the panel.
+	 * @param mode The new rendering mode.
+	 */
 	public void setRenderingMode(RenderingMode mode){
 		this.mode.update(mode);
 	}
 	
+	/**
+	 * Sets the display name of the panel.
+	 * @param name The new display name.
+	 */
 	public void setName(String name){
 		this.name.update(name);
 	}
 	
+	/**
+	 * Shows an editor to edit advanced panel properties.
+	 * @param live True if the GUI is already live and
+	 *        updates should be reflected in real time.
+	 */
 	public abstract void showEditor(boolean live);
 	
 	@Override
