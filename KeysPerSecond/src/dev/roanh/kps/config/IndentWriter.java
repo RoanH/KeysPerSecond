87261df07ec0d3d75de94bf542d5608e906c2015
Roan Hofland <roan@roanh.dev>
2024-01-13 22:54:01 +0100

    Document the IndentWriter class

--- KeysPerSecond/src/dev/roanh/kps/config/IndentWriter.java
+++ KeysPerSecond/src/dev/roanh/kps/config/IndentWriter.java
@@ -20,38 +20,81 @@ package dev.roanh.kps.config;
 
 import java.io.PrintWriter;
 
+/**
+ * Utility class used to write config files.
+ * @author Roan
+ */
 public class IndentWriter{
+	/**
+	 * The writer to write output to.
+	 */
 	private PrintWriter writer;
+	/**
+	 * The current indent of the writer in spaces.
+	 */
 	private int indent;
+	/**
+	 * True if the next println action writes the
+	 * start of a new list item.
+	 */
 	private boolean nextIsListItem = false;
 	
+	/**
+	 * Constructs a new indent writer with the
+	 * given writer to forward writes to.
+	 * @param writer The underlying writer to output to.
+	 */
 	public IndentWriter(PrintWriter writer){
 		this.writer = writer;
 	}
 	
+	/**
+	 * Starts a new list item.
+	 * @see #endListItem()
+	 */
 	public void startListItem(){
 		indent += 4;
 		nextIsListItem = true;
 	}
 	
+	/**
+	 * Ends the current list.
+	 * @see #startListItem()
+	 */
 	public void endListItem(){
 		indent -= 4;
 		nextIsListItem = false;
 	}
 	
+	/**
+	 * Increase the indent of the writer.
+	 * @see #decreaseIndent()
+	 */
 	public void increaseIndent(){
 		indent += 2;
 	}
 	
+	/**
+	 * Decreases the indent of the writer.
+	 * @see #increaseIndent()
+	 */
 	public void decreaseIndent(){
 		indent -= 2;
 	}
 	
+	/**
+	 * Prints an empty line.
+	 */
 	public void println(){
 		writer.print('\n');
 		writer.flush();
 	}
 	
+	/**
+	 * Prints a new line to the configuration subject
+	 * to the current indent and list settings.
+	 * @param val The value to write.
+	 */
 	public void println(String val){
 		for(int i = 0; i < (nextIsListItem ? (indent - 2) : indent); i++){
 			writer.print(' ');
