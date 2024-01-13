f99f400723c68379c6796d56987ee034df5ef3ec
Roan Hofland <roan@roanh.dev>
2024-01-13 23:24:39 +0100

    Document the SettingGroup class

--- KeysPerSecond/src/dev/roanh/kps/config/SettingGroup.java
+++ KeysPerSecond/src/dev/roanh/kps/config/SettingGroup.java
@@ -20,21 +20,51 @@ package dev.roanh.kps.config;
 
 import java.util.Map;
 
+/**
+ * Group of regular settings that can be handled as a single unit.
+ * @author Roan
+ */
 public abstract class SettingGroup{
+	/**
+	 * The key identifier for this group.
+	 */
 	private final String key;
 	
+	/**
+	 * Constructs a new settings group with the given identifier.
+	 * @param key The 
+	 */
 	public SettingGroup(String key){
 		this.key = key;
 	}
 	
+	/**
+	 * Gets the identifier key for this group.
+	 * @return The identifier for this group.
+	 */
 	public String getKey(){
 		return key;
 	}
 
+	/**
+	 * Parses all the settings under this group.
+	 * @param data The setting data.
+	 * @return True if default values were used during the parsing process.
+	 * @see #findAndParse(Map, Setting...)
+	 * @see Setting#parse(String)
+	 */
 	public abstract boolean parse(Map<String, String> data);
 	
+	/**
+	 * Writes all the individual settings that are contained in this group.
+	 * @param out The writer to write to.
+	 */
 	public abstract void writeItems(IndentWriter out);
 	
+	/**
+	 * Writes this entire settings group.
+	 * @param out The writer to write.
+	 */
 	public final void write(IndentWriter out){
 		out.println(key + ":");
 		out.increaseIndent();
@@ -42,6 +72,14 @@ public abstract class SettingGroup{
 		out.decreaseIndent();
 	}
 	
+	/**
+	 * Resolves the given settings by their key from the given map of
+	 * data and attempts to parse the provided data for each setting.
+	 * @param data The key-value setting data.
+	 * @param settings The settings to resolve and parse.
+	 * @return True if default values were used during the parsing process.
+	 * @see Setting#parse(String)
+	 */
 	protected boolean findAndParse(Map<String, String> data, Setting<?>... settings){
 		boolean modified = false;
 		for(Setting<?> setting : settings){
