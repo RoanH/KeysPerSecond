4327ae0d9257fba5aedfaff21bb072695218de28
Roan Hofland <roan@roanh.dev>
2024-01-14 00:29:45 +0100

    Document the ListItemConstructor

--- KeysPerSecond/src/dev/roanh/kps/config/ListItemConstructor.java
+++ KeysPerSecond/src/dev/roanh/kps/config/ListItemConstructor.java
@@ -23,12 +23,36 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
+/**
+ * Interface for functions that can construct new items for a settings list.
+ * @author Roan
+ * @param <T> The type of the constructed items.
+ * @see SettingList
+ */
 @FunctionalInterface
 public abstract interface ListItemConstructor<T extends SettingGroup>{
 
-	//null indicates invalid/malformed/error/etc
+	/**
+	 * Constructs a new list item from the given raw data.
+	 * @param data The data to parse item a new list item.
+	 * @return The result of attempting to parse the given data
+	 *         or null if the data could not be parsed at all due
+	 *         to it being invalid, malformed, or causing an error.
+	 */
 	public abstract ParsedItem<T> construct(List<String> data);
 	
+	/**
+	 * Creates a new list item constructor that first constructs a new
+	 * setting group using the supplied constructor and then calls its
+	 * parse method after the raw list item data has been turned into
+	 * a key-value map.
+	 * @param <T> The type of the constructed list items.
+	 * @param ctor The constructor to use to create new settings group objects.
+	 * @return The created list item constructor.
+	 * @see #constructThenParse(Supplier, Map)
+	 * @see SettingGroup#parse(Map)
+	 * @see #buildMap(List)
+	 */
 	public static <T extends SettingGroup> ListItemConstructor<T> constructThenParse(Supplier<T> ctor){
 		return data->{
 			Map<String, String> map = buildMap(data);
@@ -40,12 +64,26 @@ public abstract interface ListItemConstructor<T extends SettingGroup>{
 		};
 	}
 	
+	/**
+	 * Creates a new list item constructor that first constructs a new
+	 * setting group using the supplied constructor and then calls its
+	 * parse method.
+	 * @param <T> The type of the constructed list items.
+	 * @param ctor The constructor to use to create new settings group objects.
+	 * @param data The data to parse.
+	 * @return The created list item constructor.
+	 * @see SettingGroup#parse(Map)
+	 */
 	public static <T extends SettingGroup> ParsedItem<T> constructThenParse(Supplier<T> ctor, Map<String, String> data){
 		T item = ctor.get();
 		return ParsedItem.of(item.parse(data), item);
 	}
 	
-	//null for invalid/not possible
+	/**
+	 * Builds a key-value map by splitting each data item on the colon character.
+	 * @param data The data items to transform.
+	 * @return A map with key-value entries or null if not all data could be split.
+	 */
 	public static Map<String, String> buildMap(List<String> data){
 		Map<String, String> map = new HashMap<String, String>();
 		for(String line : data){
@@ -59,23 +97,54 @@ public abstract interface ListItemConstructor<T extends SettingGroup>{
 		return map;
 	}
 	
-	public static final class ParsedItem<T>{
+	/**
+	 * Result of parsing a new list item.
+	 * @author Roan
+	 * @param <T> The type of the parsed result item.
+	 */
+	public static final class ParsedItem<T extends SettingGroup>{
+		/**
+		 * True if a default value was used during parsing.
+		 */
 		private final boolean defaultUsed;
+		/**
+		 * The parsed item.
+		 */
 		private final T item;
 		
+		/**
+		 * Constructs a new parsed item.
+		 * @param defaultUsed True if a default value was used.
+		 * @param item The parsed item.
+		 */
 		private ParsedItem(boolean defaultUsed, T item){
 			this.defaultUsed = defaultUsed;
 			this.item = item;
 		}
 		
+		/**
+		 * Returns if a default value was used during item parsing.
+		 * @return True if a default value was used during parsing.
+		 */
 		public boolean defaultUsed(){
 			return defaultUsed;
 		}
 		
+		/**
+		 * Gets the item that was constructed.
+		 * @return The parsed item.
+		 */
 		public T item(){
 			return item;
 		}
-		
+
+		/**
+		 * Constructs a new parsed item.
+		 * @param <T> The type of the parsed result item.
+		 * @param defaultUsed True if a default value was used.
+		 * @param item The parsed item.
+		 * @return The parsed item.
+		 */
 		public static final <T extends SettingGroup> ParsedItem<T> of(boolean defaultUsed, T item){
 			return new ParsedItem<T>(defaultUsed, item);
 		}
