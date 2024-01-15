6eb7be7a52898b38a41e38e3ca5160bf8fd10522
Roan Hofland <roan@roanh.dev>
2024-01-15 21:54:28 +0100

    Document the LastPanelSettings class

--- KeysPerSecond/src/dev/roanh/kps/config/group/LastPanelSettings.java
+++ KeysPerSecond/src/dev/roanh/kps/config/group/LastPanelSettings.java
@@ -28,26 +28,56 @@ import dev.roanh.kps.panels.LastPanel;
 import dev.roanh.kps.ui.editor.Editor;
 import dev.roanh.kps.ui.editor.LastPanelEditor;
 
+/**
+ * Settings for a panel showing the time since the last input.
+ * @author Roan
+ * @see LastPanel
+ */
 public class LastPanelSettings extends SpecialPanelSettings{
+	/**
+	 * Maximum number of time units to show at the same time.
+	 * E.g., two units could mean showing '5m 26s'.
+	 */
 	private IntSetting units = new IntSetting("units", 1, 4, 1);
+	/**
+	 * Whether milliseconds are considered as a valid unit to show or not.
+	 * If false milliseconds are never shown.
+	 */
 	private BooleanSetting millis = new BooleanSetting("showMillis", false);
 
 	public LastPanelSettings(){
 		super(PanelType.LAST, "LST");
 	}
 	
+	/**
+	 * True if millisecond may be shown as a value for this panel.
+	 * @return True if milliseconds are considered a valid unit.
+	 */
 	public boolean showMillis(){
 		return millis.getValue();
 	}
 	
+	/**
+	 * Gets the maximum number of time units to string
+	 * together to form the time string.
+	 * @return The maximum time unit count.
+	 */
 	public int getUnitCount(){
 		return units.getValue();
 	}
 	
+	/**
+	 * Sets the maximum number of time units allowed to be used.
+	 * @param units The maximum number of time units.
+	 */
 	public void setUnitCount(int units){
 		this.units.update(units);
 	}
 	
+	/**
+	 * Sets whether millisecond resolution is allowed as a valid panel value.
+	 * @param shown True if milliseconds are allowed to be shown.
+	 */
 	public void setShowMillis(boolean shown){
 		millis.update(shown);
 	}
